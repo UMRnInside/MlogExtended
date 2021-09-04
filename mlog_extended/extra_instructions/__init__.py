44f3f3440cae62c840a9482d879b9b968d916749
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2021-09-04 23:37:02 +0800

    Support comments

--- mlog_extended/extra_instructions/__init__.py
+++ mlog_extended/extra_instructions/__init__.py
@@ -2,7 +2,14 @@ from .jump_if import jump_if
 from .extended_let import extended_let
 from .unit_control import unit_control
 
+def comment(src_line: str) -> list:
+    """Returns an empty list."""
+    return []
+
+
 INSTRUCTIONS = {
+    # Yes, comments are instructions
+    "#": comment,
     "jump-if": jump_if,
     "xlet": extended_let,
     "unit-control": unit_control,
