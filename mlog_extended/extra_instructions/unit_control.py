d8a3dd527a9cc9a99dc869274646608da142c885
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2021-09-05 09:08:19 +0800

    unit_control: Refactor and aliases

--- mlog_extended/extra_instructions/unit_control.py
+++ mlog_extended/extra_instructions/unit_control.py
@@ -11,125 +11,40 @@ def pad_args(src: list) -> list:
     result.extend(["0",]*padding_count)
     return result
 
-def command_idle(kwargs: dict) -> list:
-    idle_commands = ["ucontrol", "idle"]
-    return pad_args(idle_commands)
+COMMAND_ARGS = {
+    "idle": (),
+    "stop": (),
+    "move": ("x", "y"),
+    "approach": ("x", "y", "radius"),
+    "boost": ("enable", ),
+    "pathfind": (),
+    "target": ("x", "y", "shoot"),
+    "targetp": ("unit", "shoot"),
+    "itemDrop": ("to", "amount"),
+    "itemTake": ("from", "item", "amount"),
+    "payDrop": (),
+    "payTake": ("takeUnits", ),
+    "mine": ("x", "y"),
+    "flag": ("value", ),
+    "build": ("x", "y", "block", "rotation", "config"),
+    "getBlock": ("x", "y", "type", "building"),
+    "within": ("x", "y", "radius", "result"),
+}
 
-def command_stop(kwargs: dict) -> list:
-    commands = ["ucontrol", "stop"]
-    return pad_args(commands)
-
-def command_move(kwargs: dict) -> list:
-    commands = ["ucontrol", "move"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    return pad_args(commands)
-
-def command_approach(kwargs: dict) -> list:
-    commands = ["ucontrol", "approach"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    commands.append(kwargs["radius"])
-    return pad_args(commands)
-
-def command_boost(kwargs: dict) -> list:
-    commands = ["ucontrol", "boost"]
-    commands.append(kwargs["enable"])
-    return pad_args(commands)
-
-def command_pathfind(kwargs: dict) -> list:
-    commands = ["ucontrol", "pathfind"]
-    return pad_args(commands)
-
-def command_target(kwargs: dict) -> list:
-    commands = ["ucontrol", "target"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    commands.append(kwargs["shoot"])
-    return pad_args(commands)
-
-def command_targetp(kwargs: dict) -> list:
-    commands = ["ucontrol", "targetp"]
-    commands.append(kwargs["unit"])
-    commands.append(kwargs["shoot"])
-    return pad_args(commands)
-
-def command_itemdrop(kwargs: dict) -> list:
-    commands = ["ucontrol", "itemDrop"]
-    commands.append(kwargs["to"])
-    commands.append(kwargs["amount"])
-    return pad_args(commands)
-
-def command_itemtake(kwargs: dict) -> list:
-    commands = ["ucontrol", "itemTake"]
-    commands.append(kwargs["from"])
-    commands.append(kwargs["item"])
-    commands.append(kwargs["amount"])
-    return pad_args(commands)
-
-def command_paydrop(kwargs: dict) -> list:
-    commands = ["ucontrol", "payDrop"]
-    return pad_args(commands)
-
-def command_paytake(kwargs: dict) -> list:
-    commands = ["ucontrol", "payTake"]
-    commands.append(kwargs["takeUnits"])
-    return pad_args(commands)
-
-def command_mine(kwargs: dict) -> list:
-    commands = ["ucontrol", "mine"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    return pad_args(commands)
-
-def command_flag(kwargs: dict) -> list:
-    commands = ["ucontrol", "flag"]
-    commands.append(kwargs["value"])
-    return pad_args(commands)
-
-def command_build(kwargs: dict) -> list:
-    commands = ["ucontrol", "build"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    commands.append(kwargs["block"])
-    commands.append(kwargs["rotation"])
-    commands.append(kwargs["config"])
-    return pad_args(commands)
-
-def command_getblock(kwargs: dict) -> list:
-    commands = ["ucontrol", "getBlock"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    commands.append(kwargs["type"])
-    commands.append(kwargs["building"])
-    return pad_args(commands)
-
-def command_within(kwargs: dict) -> list:
-    commands = ["ucontrol", "within"]
-    commands.append(kwargs["x"])
-    commands.append(kwargs["y"])
-    commands.append(kwargs["radius"])
-    commands.append(kwargs["result"])
-    return pad_args(commands)
-
-COMMANDS = {
-    "idle": command_idle,
-    "stop": command_stop,
-    "move": command_move,
-    "approach": command_approach,
-    "boost": command_boost,
-    "pathfind": command_pathfind,
-    "target": command_target,
-    "targetp": command_targetp,
-    "itemDrop": command_itemdrop,
-    "itemTake": command_itemtake,
-    "payDrop": command_paydrop,
-    "payTake": command_paytake,
-    "mine": command_mine,
-    "flag": command_flag,
-    "build": command_build,
-    "getBlock": command_getblock,
-    "within": command_within
+COMMAND_ARG_ALIASES = {
+    "boost": {
+        "boost": "enable",
+    },
+    "targetp": {
+        "target": "unit",
+    },
+    "flag": {
+        "flag": "value",
+    },
+    "getBlock": {
+        "resultType": "type",
+        "resultBuilding": "building",
+    },
 }
 
 def unit_control(src_line: str) -> list:
@@ -141,11 +56,26 @@ def unit_control(src_line: str) -> list:
     result = []
 
     try:
-        if command not in COMMANDS.keys():
+        if command not in COMMAND_ARGS.keys():
             message = F"error: unsupported unit command '{command}'"
             raise CompilationError(message)
-        parser_function = COMMANDS[command]
-        result.append(" ".join(parser_function(kwargs)))
+
+        vanilla_kwargs = []
+        if command in COMMAND_ARG_ALIASES.keys():
+            aliases = COMMAND_ARG_ALIASES[command]
+            for alias in aliases.keys():
+                if alias in kwargs.keys():
+                    vanilla_kwargs.append((aliases[alias], kwargs[alias]))
+        for (k, v) in vanilla_kwargs:
+            kwargs[k] = v
+
+        output_verdicts = []
+        output_verdicts.append("ucontrol")
+        output_verdicts.append(command)
+        for key in COMMAND_ARGS[command]:
+            output_verdicts.append(kwargs[key])
+        output_verdicts = pad_args(output_verdicts)
+        result.append(" ".join(output_verdicts))
     except KeyError as exception:
         name = ""
         if len(exception.args) >= 1:
--- tests/test_unit_control.py
+++ tests/test_unit_control.py
@@ -24,7 +24,7 @@ unit-control payDrop
 unit-control payTake takeUnits=myUnit
 unit-control mine x=128 y=192
 unit-control flag value=10000
-unit-control getBlock x=1 y=2 type=0 building=resultBuilding
+unit-control getBlock x=1 y=2 type=type building=resultBuilding
 unit-control within x=1 y=2 radius=3 result=isWithinRadius
 """
         ans = """ucontrol idle 0 0 0 0 0
@@ -41,8 +41,24 @@ ucontrol payDrop 0 0 0 0 0
 ucontrol payTake myUnit 0 0 0 0
 ucontrol mine 128 192 0 0 0
 ucontrol flag 10000 0 0 0 0
-ucontrol getBlock 1 2 0 resultBuilding 0
+ucontrol getBlock 1 2 type resultBuilding 0
 ucontrol within 1 2 3 isWithinRadius 0
+"""
+        self.assertEqual(ans.splitlines(),
+                compiler.compile(src_text).splitlines())
+
+    def test_unit_control_aliases(self):
+        compiler = ExtendedCompiler()
+        src_text = """
+unit-control boost boost=1
+unit-control targetp target=enemy shoot=shooting
+unit-control flag flag=10000
+unit-control getBlock x=1 y=2 resultType=type resultBuilding=building
+"""
+        ans = """ucontrol boost 1 0 0 0 0
+ucontrol targetp enemy shooting 0 0 0
+ucontrol flag 10000 0 0 0 0
+ucontrol getBlock 1 2 type building 0
 """
         self.assertEqual(ans.splitlines(),
                 compiler.compile(src_text).splitlines())
