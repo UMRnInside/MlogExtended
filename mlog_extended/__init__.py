ae6ee14539e63c0069ec0dc25af2ea4e8a08c66a
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2021-09-11 19:25:34 +0800

    Version 0.0.3

--- mlog_extended/__init__.py
+++ mlog_extended/__init__.py
@@ -3,7 +3,3 @@
 from .compilation_error import CompilationError
 from .basic_compiler import BasicCompiler
 from .extended_compiler import ExtendedCompiler 
-
-#CompilationError = _mlogex_bc.CompilationError
-#BasicCompiler = _mlogex_bc.BasicCompiler
-#InstructionExtendedCompiler = _mlogex_iec.InstructionExtendedCompiler
